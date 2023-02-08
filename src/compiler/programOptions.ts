66beecf894c7672fb4bd98154173e3465e8f247b
meh <meh@meh>
2023-02-08 07:25:16 +0700

    init `src\compiler\programOptions.ts`

---
+++ src/compiler/programOptions.ts
@@ -0,0 +1,92 @@
+import {
+    BaseNodeFactory,
+    CreateSourceFileOptions,
+    EmitHelperFactory,
+    GetCanonicalFileName,
+    MapLike,
+    ModeAwareCache,
+    ModeAwareCacheKey,
+    ModuleResolutionCache,
+    MultiMap,
+    NodeFactoryFlags,
+    OptionsNameMap,
+    PackageJsonInfo,
+    PackageJsonInfoCache,
+    Pattern,
+    ProgramBuildInfo,
+    Push,
+    SymlinkCache,
+    ThisContainer,
+} from "./_namespaces/ts";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
