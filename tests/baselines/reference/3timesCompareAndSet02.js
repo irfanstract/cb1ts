1acb4ce4879ecdf5172b0c45b84fd95a2dc09ae5
meh <meh@meh>
2023-02-10 09:35:18 +0700

    add `tests\baselines\reference\3timesCompareAndSet02.*`

---
+++ tests/baselines/reference/3timesCompareAndSet02.js
@@ -0,0 +1,45 @@
+//// [3timesCompareAndSet02.ts]
+const example = (
+    new (class Incr {
+
+        value !: number ;
+        constructor() {
+            this.value = 0 ;
+        }
+        
+    } )()
+) ;
+{
+    /**  
+     * shall all complain about missing argument-list
+     */
+    {
+        example.value __compareAndSet ;
+        // example.value __compareAndSet ;
+        // example.value __compareAndSet ;
+    }
+}
+
+
+
+
+
+
+
+//// [3timesCompareAndSet02.js]
+var example = (new (/** @class */ (function () {
+    function Incr() {
+        this.value = 0;
+    }
+    return Incr;
+}()))());
+{
+    /**
+     * shall all complain about missing argument-list
+     */
+    {
+        ;
+        // example.value __compareAndSet ;
+        // example.value __compareAndSet ;
+    }
+}
