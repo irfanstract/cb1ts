61f00310113626f01446ddf2e579852a39f572ca
meh <meh@meh>
2023-02-10 07:46:05 +0700

    add `tests\cases\compiler\3timesCompareAndSet01.ts`

---
+++ tests/cases/compiler/3timesCompareAndSet01.ts
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+const example = (
+    new (class Incr {
+
+        value !: number ;
+        constructor() {
+            this.value = 0 ;
+        }
+        
+        increment(): void {
+            const {
+                value: existingValue ,
+            } = this ;
+            this.value __compareAndSet(existingValue, existingValue + 1 );
+        }
+
+    } )()
+) ;
+{
+    const {
+        value: val0,
+    } = example ;
+    example.increment() ;
+    example.increment() ;
+    const {
+        value: val2,
+    } = example ;
+    // console["info"]({ value: example.value , }) ;
+    if (val0 + 2 === val2 ) {
+    } else {
+        throw TypeError(`failed to ensure 'val0 + 2 === val2' `) ;
+    }
+}
+
+
+
+
+
