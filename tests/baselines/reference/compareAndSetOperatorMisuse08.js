7e35e0955cb9eca90da38144d512acdc0d710a5a
meh <meh@meh>
2023-02-11 19:34:33 +0700

    Test - CompareAndSetOperatorMisuse08 - Output Baseline

---
+++ tests/baselines/reference/compareAndSetOperatorMisuse08.js
@@ -0,0 +1,54 @@
+//// [compareAndSetOperatorMisuse08.ts]
+const example = {
+    bar: (
+        new (class Incr {
+    
+            value !: number ;
+            constructor() {
+                this.value = 0 ;
+            }
+            
+        } )()
+    ) ,
+} ;
+{
+    {
+        /** 
+         * ambiguous, in face of ASI (Automatic Semicolon Insertion).
+         * 
+         */
+        example
+            .bar
+            .value 
+            __compareAndSet(...[0, 1] ) // ill-formed
+        ;
+    }
+}
+
+
+
+
+
+
+
+//// [compareAndSetOperatorMisuse08.js]
+var example = {
+    bar: (new (/** @class */ (function () {
+        function Incr() {
+            this.value = 0;
+        }
+        return Incr;
+    }()))()),
+};
+{
+    {
+        /**
+         * ambiguous, in face of ASI (Automatic Semicolon Insertion).
+         *
+         */
+        example
+            .bar
+            .value;
+        __compareAndSet.apply(void 0, [0, 1]);
+    }
+}
