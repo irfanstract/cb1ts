6f3613bc78ed02ced77fdbbbb393498b11d5ffb1
meh <meh@meh>
2023-02-11 17:49:05 +0700

    Test - CompareAndSetOperatorMisuse05 - Output Baseline

---
+++ tests/baselines/reference/compareAndSetOperatorMisuse05.js
@@ -0,0 +1,25 @@
+//// [compareAndSetOperatorMisuse05.ts]
+{
+    /**  
+     * this shall raise syntax error exactly "__compareAndSet requires two-val args-list and cannot be followed by assignment operators".
+     */
+    {
+        example.value __compareAndSet += "5" ;
+    }
+}
+
+
+
+
+
+
+
+//// [compareAndSetOperatorMisuse05.js]
+{
+    /**
+     * this shall raise syntax error exactly "__compareAndSet requires two-val args-list and cannot be followed by assignment operators".
+     */
+    {
+         += "5";
+    }
+}
