e299dd4fb14be3b4849e072a87ae3c89a07a7191
meh <meh@meh>
2023-02-11 17:48:37 +0700

    Test - CompareAndSetOperatorMisuse05 - Code

---
+++ tests/cases/compiler/compareAndSetOperatorMisuse05.ts
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+{
+    /**  
+     * this shall raise syntax error exactly "__compareAndSet requires two-val args-list and cannot be followed by assignment operators".
+     */
+    {
+        example.value __compareAndSet += "5" ;
+    }
+}
+
+
+
+
+
