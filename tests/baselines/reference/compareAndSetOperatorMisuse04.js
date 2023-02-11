80efb5d0e80f8475462daaea6980f661c921c86c
meh <meh@meh>
2023-02-11 15:48:45 +0700

    Test Baseline Reference - CompareAndSetOperatorMisuse04

---
+++ tests/baselines/reference/compareAndSetOperatorMisuse04.js
@@ -0,0 +1,42 @@
+//// [compareAndSetOperatorMisuse04.ts]
+const example = (
+    new (class Incr {
+
+        value !: number ;
+        constructor() {
+            this.value = 0 ;
+        }
+        
+    } )()
+) ;
+{
+    /**  
+     * shall raise syntax error exactly "__compareAndSet cannot be tailed by anything other than [...]"
+     */
+    {
+        example.value __compareAndSet console.info( ) ;
+    }
+}
+
+
+
+
+
+
+
+//// [compareAndSetOperatorMisuse04.js]
+var example = (new (/** @class */ (function () {
+    function Incr() {
+        this.value = 0;
+    }
+    return Incr;
+}()))());
+{
+    /**
+     * shall raise syntax error exactly "__compareAndSet cannot be tailed by anything other than [...]"
+     */
+    {
+        ;
+        console.info();
+    }
+}
