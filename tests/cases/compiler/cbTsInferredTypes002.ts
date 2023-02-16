a5dd80f7497d82077b57632244596b5380887bd5
meh <meh@meh>
2023-02-17 01:14:21 +0700

    test case - CBTS Inferred Types 002

---
+++ tests/cases/compiler/cbTsInferredTypes002.ts
@@ -0,0 +1,41 @@
+// @inferredTypeSpecificity: medium
+
+
+
+
+
+
+const string1 = "hello" ;
+    
+const literal2 = "world" ;
+
+const majorVersion = 3 ;
+
+const minorVersions = [0, 1, 2, 3, ] ;
+
+const minorVersionsAgain = [0, 1, 2, 3, ] satisfies [unknown, ...unknown[] ] ;
+
+const minorVersionsAgainAgain = minorVersionsAgain ;
+
+const minorVersionsAgain3 = [0, 1, 2, 3, ] satisfies [0, unknown, 2, ...unknown[] ] ;
+
+const desc = {
+    type: "color",
+    value: "blue",
+    specificity: 3,
+} ;
+const desc2 = {
+    type: "color",
+    value: "blue",
+    specificity: 3,
+} satisfies {
+    type: "color",
+    value: "blue" | "red" | "yellow" | "green",
+    specificity: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 ,
+} ;
+
+const value11 = 5 + majorVersion ;
+const string12 = `version ${ majorVersion }` ;
+
+
+
