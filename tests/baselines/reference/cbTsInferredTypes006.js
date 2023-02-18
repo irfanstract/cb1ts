4d32cdb5a97d1c6109150beaa0525f0a175fa877
meh <meh@meh>
2023-02-18 07:52:54 +0700

    test case - CBTS Inferred Types 006

---
+++ tests/baselines/reference/cbTsInferredTypes006.js
@@ -0,0 +1,50 @@
+//// [cbTsInferredTypes006.ts]
+// 
+
+// deliberately not setting `inferredTypeSpecificity` in this case
+
+
+
+
+
+
+{
+    const string1 = "hello" ;
+    
+    const stringStrong11: "hello" | "hi" = "hi" ;
+        
+}
+{
+    let string1 = "hello" ;
+    
+    let stringStrong11: "hello" | "hi" = "hi" ;
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// [cbTsInferredTypes006.js]
+// 
+// deliberately not setting `inferredTypeSpecificity` in this case
+{
+    var string1 = "hello";
+    var stringStrong11 = "hi";
+}
+{
+    var string1 = "hello";
+    var stringStrong11 = "hi";
+}
