5fbc1faabf9b8a0e3bfb296e8bf1cb21f9ab9a52
meh <meh@meh>
2023-02-20 19:31:48 +0700

    `WideningContext` - remove `primitiveOrObjectOrArrayLiteralTypeWideningRequired`

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -6832,7 +6832,6 @@ export interface WideningContext {
     propertyName?: __String;        // Name of property in parent
     siblings?: Type[];              // Types of siblings
     resolvedProperties?: Symbol[];  // Properties occurring in sibling object literals
-    primitiveOrObjectOrArrayLiteralTypeWideningRequired: 0 | 1;
 }
 
 /** @internal */
