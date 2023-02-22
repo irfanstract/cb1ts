98935ee13f77c592f6b72aa0afcf7b8d8d20056c
meh <meh@meh>
2023-02-22 15:58:05 +0700

    Revert `1209bf6ab0a203ec29a0e8cc06e1ead05c3447dc..dea5a8dc2762e52f66a6dd6a8ce04971f99a45c1`

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -6161,21 +6161,7 @@ export function createTypeChecker(host: TypeCheckerHost): TypeChecker {
         // Otherwise, we always strip comments out.
         const printer = type !== unresolvedType ? createPrinterWithRemoveComments() : createPrinterWithDefaults();
         const sourceFile = enclosingDeclaration && getSourceFileOfNode(enclosingDeclaration);
-        if ((
-            false
-            || isCbTsValueofType(type)
-            // || (type.flags & TypeFlags.ESSymbolLike)
-            // || (type.flags & TypeFlags.Object)
-        )) {
-            writer.write(`/* id=0x${(type.id satisfies number).toString(0x10) } */ `) ;
-        }
         printer.writeNode(EmitHint.Unspecified, typeNode, /*sourceFile*/ sourceFile, writer);
-        if ((
-            false
-            || isCbTsValueofType(type)
-        )) {
-            writer.write(` /* BaseType: ${typeToString(getTypeOfSymbol(type.symbol)) } */`) ;
-        }
         const result = writer.getText();
 
         const maxLength = noTruncation ? noTruncationMaximumTruncationLength * 2 : defaultMaximumTruncationLength * 2;
